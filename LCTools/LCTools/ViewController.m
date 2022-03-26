3b30280b9afd5a58532134862a3b5e629a981401
muziwange <694034632@qq.com>
2022-03-26 16:59:25 +0800

    done

--- LCTools/LCTools/ViewController.m
+++ LCTools/LCTools/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import "PAEHasHeaderBaseVC.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [btn addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    [btn setTitle:@"点我" forState:UIControlStateNormal];
+    btn.frame = CGRectMake(50, 100, 100, 50);
 }
 
+- (void)click {
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:[PAEHasHeaderBaseVC new]];
+    [self presentViewController:nav animated:YES completion:nil];
+}
 
 @end
